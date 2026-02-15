54f904b28f778ad80edf2e4697cf30ea16c98195
Marbru35 <marlon.spiess@outlook.com>
2026-02-15 14:30:13 +0100

    deleted unused routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-
 import HomePage from "./pages/HomePage.jsx";
-import DatenschutzPage from "./pages/DatenschutzPage.jsx";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/datenschutz" element={<DatenschutzPage />} />
-      <Route path="*" element={<HomePage />} />
-    </Routes>
-  );
+  return <HomePage />;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import "./css/App.css";
@@ -14,8 +13,6 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
